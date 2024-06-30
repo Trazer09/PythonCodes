93e281455d9369c0c99493c852d122833a83a621
Sidhant <84363346+Trazer09@users.noreply.github.com>
2024-06-30 13:40:13 +0530

    added 4.if_else.py

---
+++ Learning Python/Learn the basics/4.if_else.py
@@ -0,0 +1,37 @@
+
+class Solution:
+    def compareNM(self, n : int, m : int) -> str:
+        
+        # code here
+        if n>m:
+            return "greater"
+        
+        elif m>n:
+            return "lesser"
+        
+        else:
+            return "equal"
+    
+        
+
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        m = int(input())
+        
+        obj = Solution()
+        res = obj.compareNM(n, m)
+        
+        print(res)
+        
+
+# } Driver Code Ends
